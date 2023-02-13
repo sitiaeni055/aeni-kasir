dd9304219d8f7dccb05e4dbdb1005bcc85ea3475
sitiaeni055 <sitiaenipaujiah555@gmail.com>
2023-02-13 14:47:30 +0700

    up

--- admin/product_list.php
+++ admin/product_list.php
@@ -25,8 +25,8 @@
             <td><?php echo $row["stok"]?></td>
             <td><img width="70" src="../image/<?php echo $row["gambar"]?>" alt=""></td>
             <td>
-                <a href="home.php?id=<?php echo $row['id']?>&halaman=delete"><button type="submit" class="btn btn-primary">Delete</button></a>
-                <a href="home.php?id=<?php echo $row['id']?>&halaman=edit"><button type="submit" class="btn btn-danger">Edit</button></a>
+                <a href="home.php?id=<?php echo $row['id']?>&halaman=delete"><button type="submit" class="btn btn-danger">Delete</button></a>
+                <a href="home.php?id=<?php echo $row['id']?>&halaman=edit"><button type="submit" class="btn btn-primary">Edit</button></a>
             
             </td>
         </tr>
